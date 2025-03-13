f7911b5a6208f7d239c83b5ac0955655367039bb
LittIeLove <breno.bso69@gmail.com>
2025-03-13 19:57:57 +0000

    salvar

---
+++ for_exemplos.py
@@ -0,0 +1,7 @@
+import os
+os.system ("clear || cls")
+
+print("Com laço de repetição")
+for i in range (1,11):
+    print(i)
+    
---
+++ for_exemplos_2.py
@@ -0,0 +1,11 @@
+import os
+import time
+os.system ("clear || cls")
+print("Contagem de 2 em 2")
+
+#Contagem
+for i in range(0,11,2):
+    print(f"Valor da variavel i: {i}")
+    time.sleep(1) #Timer
+
+print("Acabou")
