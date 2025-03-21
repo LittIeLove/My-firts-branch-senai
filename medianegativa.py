45c4c4eaebd3041c17984198328b58dc452d441d
LittIeLove <breno.bso69@gmail.com>
2025-03-21 22:47:41 +0000

    salvando

--- medianegativa.py
+++ medianegativa.py
@@ -1,23 +1,39 @@
 import os
 os.system ("clear || cls")
 from time import sleep
+nome = input('Digite seu nome: ')
 sequenciaf = 1
-nota = 0
 nota = float(input("Digite sua primeira nota: "))
+notaf = nota
 if nota < 0:
-        print("Apenas números positivos\nRecarregando")
-        nota = 0
-        sleep(2)
+    print("Apenas números positivos\nSaindo")
+    nota = 0
+    sleep(2)
+    exit()
 
-while True:
-    sequencia = int(input("Deseja inserir outra nota ? (1) para sim / (qualquer outro numero) para não : "))
-    if sequencia == 1:
+while True :
+    sequencia = int(input('Você deseja adicionar mais uma nota ? (1) para sim e (qualquer numero) para não: '))
+    if sequencia ==1 :
         sequenciaf += 1
-        nota += float(input(f"Digite sua {sequenciaf}ª nota"))
-    elif nota < 0:
-        print("Apenas números positivos\nRecarregando")
-        nota = 0
-        sleep(2)
+        nota = float(input(f"Digite sua a {sequenciaf} nota: "))
+        notaf += nota
+        if nota < 0:
+            os.system ('clear')
+            print('Nota negativa\nSequencia encerrada')
+            sleep(3)
+            break
+    else:
         break
 
-    
+media = notaf / sequenciaf
+
+
+print(f'Nome: {nome}')
+print(f'Quantidade de nota: {sequenciaf}')
+print(f'Sua media: {media:.2f}')
+
+
+
+
+
+
