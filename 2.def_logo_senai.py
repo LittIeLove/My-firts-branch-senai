69d1130e9659e9a370f8b4efdef85b340c45b39a
LittIeLove <breno.bso69@gmail.com>
2025-04-03 18:04:29 +0000

    salvando

---
+++ 2.def_logo_senai.py
@@ -0,0 +1,16 @@
+import os
+os.system  ("clear || cls")
+
+def logo_senai():
+    os.system  ("clear || cls")
+    print("=== SENAI ===")
+
+logo_senai()
+nome = input("Digite seu nome: ")
+
+logo_senai()
+idade = input("Digite sua idade: ")
+
+logo_senai()
+print(f"\nNome: {nome}")
+print(f"Idade: {idade}")
