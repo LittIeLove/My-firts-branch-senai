1d408008b51a4f830e96ef86d2f299839b3cea40
LittIeLove <breno.bso69@gmail.com>
2025-04-10 19:56:55 +0000

    salvando

---
+++ vetores2.py
@@ -0,0 +1,32 @@
+import os
+os.system ("clear || cls")
+
+
+notas = []
+
+for i in range(1,5):
+    nota = float(input(f"Digite sua {i}ª nota: "))
+    notas.append(nota)
+
+def media (notas):
+    soma = sum(notas)
+    mediaf = soma / 4
+    return mediaf
+
+media_final = media(notas)
+
+def resultado (media_final):
+    if media_final >= 7:
+        return f"Aprovado\nNotas: {notas}\nMedia: {media_final}"
+    
+    elif media_final >= 5:
+        return f"Recuperação\nNotas: {notas}\nMedia: {media_final}"
+
+    else:
+        return f"Reprovado\nNotas: {notas}\nMedia: {media_final}"
+    
+resultado(media_final)
+
+
+    
+        
